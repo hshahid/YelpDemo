373b5ab14ff9dfe0a8495bcbe6322d39e1d72321
Hashma <shahi22h@mtholyoke.edu>
2015-08-25 16:17:25 -0400

    Link reviews to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825155752) do
+ActiveRecord::Schema.define(version: 20150825164320) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20150825155752) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
